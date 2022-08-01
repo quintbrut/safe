c5ac96241dda69e9c5750c388c25c258ab1cb9c6
markbrut <themarkbrut@gmail.com>
2022-08-01 17:50:23 +0600

    revert extreme

--- safe.py
+++ safe.py
@@ -89,7 +89,7 @@ def unlimitedSafer():
 def main():
     print('Starting ...')
     login('weakjoker5', '7shokpar7')
-    for _ in range(200):
+    for _ in range(30):
         threading.Thread(target=unlimitedSafer).start()
 
 
