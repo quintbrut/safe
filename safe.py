9eec47847931b1476bdea61dae4125f9c1744aa5
markbrut <themarkbrut@gmail.com>
2022-08-01 17:32:09 +0600

    rerange 3

--- safe.py
+++ safe.py
@@ -89,7 +89,7 @@ def unlimitedSafer():
 def main():
     print('Starting ...')
     login('weakjoker5', '7shokpar7')
-    for _ in range(50):
+    for _ in range(40):
         threading.Thread(target=unlimitedSafer).start()
 
 
