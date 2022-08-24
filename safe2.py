fc97076cb1744ab9d241462fd14d76a7c6eef79f
markbrut <themarkbrut@gmail.com>
2022-08-24 11:37:37 +0600

    add3

--- safe2.py
+++ safe2.py
@@ -37,9 +37,9 @@ def getSafeData(session):
     safe_page = session.get(safe_url)
     soup = bs(safe_page.content, "lxml")
     in_safe = int(
-        soup.find('div', class_="center font14 bold").find('img', alt='доценты').findParent().getText().strip().replace(' ',''))
+        soup.find('div', class_="center font14 bold").find('img', alt='доценты').findParent().getText().strip().replace(' ','').replace('\xa',''))
     token = soup.find('input', attrs={'type': 'hidden', 'name': 'token'})['value']
-    in_pocket = int(soup.find('span', id='res-docents').getText().strip().replace(' ',''))
+    in_pocket = int(soup.find('span', id='res-docents').getText().strip().replace(' ','').replace('\xa',''))
     print('В сейфе: ', in_safe)
     print('В кармане: ', in_pocket)
     print('Токен: ', token)
